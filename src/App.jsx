0590d04e2c7743f3309cdcc4fe8f134734af0112
lozanomasuno <lozanomasuno@gmail.com>
2024-01-28 15:49:43 -0500

    03 Ascending List mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardActions from '@mui/material/CardActions';
 import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Button from '@mui/material/Button';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+
 function App(){
   
   const [characters, setCharacters] = useState([])
@@ -16,19 +16,26 @@ function App(){
   useEffect(()=>{
     async function fetchData(){
       const response = await fetch('https://rickandmortyapi.com/api/character');
-      const data = await response.json();     
+      const data = await response.json();  
       setCharacters(data.results);
-    }
+    } 
 
     fetchData();
   }, []);
   
+  const namesAscending = [...characters].sort((a, b) =>
+   a.name > b.name ? 1 : -1,
+  );
+  console.log(namesAscending);
  
   return <>
-    <h1>Rick and Morty Characters</h1>
+    <div className="pic-banner">
+      <img src="https://i.pinimg.com/564x/43/91/5f/43915f55bc12c4890361fb5f3cecf93c.jpg" alt="main-banner" />
+    </div>
+
     <div className="container"> 
     {
-      characters.map(character => {
+      namesAscending.map(character => {
         return(
           <Card sx={{ maxWidth: 250 }} key={character.id} className="card">
             <CardHeader className="cut-text"          
@@ -45,8 +52,9 @@ function App(){
           
              <CardActions disableSpacing>
                 <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
+                  <FavoriteBorderOutlinedIcon />
                 </IconButton>
+                <Button size="small">Learn More</Button>
              </CardActions>
             </Card>
         )
